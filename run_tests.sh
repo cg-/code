40fe7c6603f77d68a8c446ca745cce373694a161
Cole Grim <cgrim@ucsc.edu>
2020-02-17 22:38:15 -0700

    bugfix tmp shared

--- run_tests.sh
+++ run_tests.sh
@@ -3,8 +3,9 @@ for i in 1 2 4 8 16 32 64
 do
 	for j in $(seq 1 $i)
 	do
-		echo Launched $i-$j > /opt/test/results/$j
-		#docker run -v /tmp:/test -v /opt/test/results:/results --rm -e TESTNO=$i-$j --name test$i-$j mine &
+		echo Launched $i-$j
+		mkdir /tmp/$i-$j
+		docker run -v /tmp/$i-$j:/test -v /opt/test/results:/results --rm -e TESTNO=$i-$j --name test$i-$j mine &
 		pids[${j}]=$!
 	done
 
