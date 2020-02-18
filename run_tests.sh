597d42f3c891048d0b53eda87eafa7654d11c95b
Cole Grim <cgrim@ucsc.edu>
2020-02-17 21:48:54 -0700

    more fixes

--- run_tests.sh
+++ run_tests.sh
@@ -3,8 +3,8 @@ for i in 1 2 4 8 16 32 64
 do
 	for j in $(seq 1 $i)
 	do
-		echo Launched $i-$j
-		docker run -v /tmp:/test -v /users/cgrim/code/results:/results --rm -e TESTNO=$i-$j --name test$i-$j mine &
+		echo Launched $i-$j > /opt/test/results/$j
+		#docker run -v /tmp:/test -v /opt/test/results:/results --rm -e TESTNO=$i-$j --name test$i-$j mine &
 		pids[${j}]=$!
 	done
 
@@ -13,6 +13,6 @@ do
 		wait $pid
 	done
 	tar -zcvf $(hostname)-results-$i.tgz results/*
-	scp -o "StrictHostKeyChecking=no" $(hostname)-results.tgz cgrim@ssh.soe.ucsc.edu:~/results/
+	scp -o "StrictHostKeyChecking=no" $(hostname)-results-$i.tgz cgrim@ssh.soe.ucsc.edu:~/results/
 done
 
